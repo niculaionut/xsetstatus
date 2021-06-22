a169006b2994ae977015f6fb03b0c0c25188d0a3
Nicula Ionut <niculaionut@tutanota.com>
2021-06-23 00:31:23 +0300

    rearrange code

--- xsetstatus.cpp
+++ xsetstatus.cpp
@@ -9,15 +9,13 @@
 #include <X11/Xlib.h>
 #endif
 
-// stl/fmt wrappers
-
+/* stl wrappers */
 static auto find_if(const auto& container, const auto pred)
 {
         return std::find_if(std::begin(container), std::end(container), pred);
 }
 
-// config variables and root strings array
-
+/* global constexpr variables */
 static constexpr int N_FIELDS = 8;
 static constexpr int FIELD_MAX_LENGTH = 22;
 static constexpr int ROOT_BUFSIZE = N_FIELDS * FIELD_MAX_LENGTH;
@@ -41,9 +39,24 @@ static constexpr auto fmt_format_buf = []()
 }();
 static constexpr std::string_view fmt_format_sv(fmt_format_buf.data());
 
+/* using declarations */
 using field_buffer_t = FixedStr<FIELD_MAX_LENGTH>;
 using root_str_buffer_t = FixedStr<ROOT_BUFSIZE>;
 
+/* enums */
+enum ResponseRootIdx
+{
+        R_TIME = 0,
+        R_LOAD,
+        R_TEMP,
+        R_VOL,
+        R_MIC,
+        R_MEM,
+        R_LANG,
+        R_DATE
+};
+
+/* global variables */
 static std::array<field_buffer_t, N_FIELDS> rootstrings = {};
 static volatile sig_atomic_t last_sig = -1;
 static volatile sig_atomic_t running = 1;
@@ -54,97 +67,12 @@ static int screen;
 static Window root;
 #endif
 
-// xsetroot utils
-
-static root_str_buffer_t get_root_string()
-{
-        root_str_buffer_t buf;
-
-        const auto format_res = std::apply(
-            [&](auto&&... args)
-            {
-                    return fmt::format_to_n(buf.data(), std::size(buf), fmt_format_sv, args.data()...);
-            },
-            rootstrings);
-        *format_res.out = '\0';
-
-        return buf;
-}
-
-static void set_root()
-{
-        const auto cstatus = get_root_string();
-
-#ifndef NO_X11
-        XStoreName(dpy, root, cstatus.data());
-        XFlush(dpy);
-#else
-        fmt::print("{}\n", cstatus.data());
-#endif
-}
-
-static void xss_exit(const int rc)
-{
-#ifndef NO_X11
-        XCloseDisplay(dpy);
-#endif
-        std::exit(rc);
-}
-
-// utilities for executing shell commands
-
+/* structs */
 struct CmdResult
 {
         const field_buffer_t output;
         const int rc;
 };
-
-template<bool OMIT_NEWLINE>
-static CmdResult exec_cmd(const char* cmd)
-{
-        field_buffer_t buf;
-
-        FILE* pipe = popen(cmd, "r");
-        if(!pipe)
-        {
-                xss_exit(EXIT_FAILURE);
-        }
-
-        fgets(buf.data(), std::size(buf), pipe);
-        buf.set_size();
-        const int rc = pclose(pipe);
-
-        if constexpr(OMIT_NEWLINE)
-        {
-                if(!buf.empty() && buf.back() == '\n')
-                {
-                        buf.pop_back();
-                }
-        }
-
-        return {buf, rc};
-}
-
-// functions for builtin responses
-
-static field_buffer_t toggle_lang()
-{
-        static constexpr field_buffer_t ltable[2] = {
-            {"US"},
-            {"RO"}
-        };
-        static constexpr const char* commands[2] = {
-            "setxkbmap us; setxkbmap -option numpad:mac",
-            "setxkbmap ro -variant std"
-        };
-        static bool flag = true;
-
-        flag = !flag;
-        std::system(commands[flag]);
-        return ltable[flag];
-}
-
-// Responses/handlers for signals and interval-updated fields
 
 struct ShellResponse
 {
@@ -166,6 +94,66 @@ public:
         const int pos;
 };
 
+/* template function declarations*/
+template<bool OMIT_NEWLINE>
+static CmdResult exec_cmd(const char* cmd);
+
+/* function declarations */
+static root_str_buffer_t get_root_string();
+static void set_root();
+static void xss_exit(const int rc);
+static field_buffer_t toggle_lang();
+static void setup();
+static void handle_sig(const int sig);
+static bool already_running();
+static void u_sig_handler(const int sig);
+static void terminator(const int);
+static void init_signals();
+static void init_statusbar();
+static void run_interval_responses();
+static void solve_signals();
+
+/* signal configs  */
+static constexpr ShellResponse rtable[] = {
+/*        shell command/script   index in root array */
+        { "xss-get-time",        R_TIME },
+        { "xss-get-load",        R_LOAD },
+        { "xss-get-temp",        R_TEMP },
+        { "xss-get-vol",         R_VOL  },
+        { "xss-get-mic",         R_MIC  },
+        { "xss-get-mem",         R_MEM  },
+        { "xss-get-date",        R_DATE },
+};
+
+static constexpr BuiltinResponse brtable[] = {
+/*        pointer to function (handler)   root array index */
+        { toggle_lang,                    R_LANG }
+};
+
+static constexpr ShellResponse interval_responses[] = {
+        rtable[0],
+        rtable[1],
+        rtable[2],
+        rtable[5],
+};
+
+static const std::pair<int, ShellResponse> sig_shell_responses[] = {
+/*        signal value    ShellResponse instance */
+        { SIGOFFSET - 1,  rtable[3] },
+        { SIGOFFSET - 2,  rtable[4] }
+};
+
+static const std::pair<int, BuiltinResponse> sig_builtin_responses[] = {
+/*        signal value    BuiltinResponse instance */
+        { SIGOFFSET - 3,  brtable[0] }
+};
+
+static const std::pair<int, void (*)()> sig_group_responses[] = {
+/*        signal value    pointer to function */
+        { SIGOFFSET - 4,  run_interval_responses}
+};
+
+/* function definitions */
 void ShellResponse::resolve() const
 {
         if(pos < 0 || pos >= N_FIELDS)
@@ -194,65 +182,85 @@ void BuiltinResponse::resolve() const
         std::strcpy(rootstrings[pos].data(), returnstr.data());
 }
 
-enum ResponseRootIdx
+template<bool OMIT_NEWLINE>
+CmdResult exec_cmd(const char* cmd)
 {
-        R_TIME = 0,
-        R_LOAD,
-        R_TEMP,
-        R_VOL,
-        R_MIC,
-        R_MEM,
-        R_LANG,
-        R_DATE
-};
+        field_buffer_t buf;
 
-static constexpr ShellResponse rtable[] = {
-/*        shell command/script   index in root array */
-        { "xss-get-time",        R_TIME },
-        { "xss-get-load",        R_LOAD },
-        { "xss-get-temp",        R_TEMP },
-        { "xss-get-vol",         R_VOL  },
-        { "xss-get-mic",         R_MIC  },
-        { "xss-get-mem",         R_MEM  },
-        { "xss-get-date",        R_DATE },
-};
-
-static constexpr BuiltinResponse brtable[] = {
-/*        pointer to function (handler)   root array index */
-        { toggle_lang,                    R_LANG }
-};
-
-static constexpr ShellResponse interval_responses[] = {
-        rtable[0],
-        rtable[1],
-        rtable[2],
-        rtable[5],
-};
-
-static const std::pair<int, ShellResponse> sig_shell_responses[] = {
-/*        signal value    ShellResponse instance*/
-        { SIGOFFSET - 1,  rtable[3] },
-        { SIGOFFSET - 2,  rtable[4] }
-};
-
-static const std::pair<int, BuiltinResponse> sig_builtin_responses[] = {
-/*        signal value    BuiltinResponse instance*/
-        { SIGOFFSET - 3,  brtable[0] }
-};
-
-static void run_interval_responses()
-{
-        for(const auto& r : interval_responses)
+        FILE* pipe = popen(cmd, "r");
+        if(!pipe)
         {
-                r.resolve();
+                xss_exit(EXIT_FAILURE);
         }
+
+        fgets(buf.data(), std::size(buf), pipe);
+        buf.set_size();
+        const int rc = pclose(pipe);
+
+        if constexpr(OMIT_NEWLINE)
+        {
+                if(!buf.empty() && buf.back() == '\n')
+                {
+                        buf.pop_back();
+                }
+        }
+
+        return {buf, rc};
 }
 
-static const std::pair<int, void (*)()> sig_group_responses[] = {
-        { SIGOFFSET - 4,  run_interval_responses}
-};
+root_str_buffer_t get_root_string()
+{
+        root_str_buffer_t buf;
 
-static void setup()
+        const auto format_res = std::apply(
+            [&](auto&&... args)
+            {
+                    return fmt::format_to_n(buf.data(), std::size(buf), fmt_format_sv, args.data()...);
+            },
+            rootstrings);
+        *format_res.out = '\0';
+
+        return buf;
+}
+
+void set_root()
+{
+        const auto cstatus = get_root_string();
+
+#ifndef NO_X11
+        XStoreName(dpy, root, cstatus.data());
+        XFlush(dpy);
+#else
+        fmt::print("{}\n", cstatus.data());
+#endif
+}
+
+void xss_exit(const int rc)
+{
+#ifndef NO_X11
+        XCloseDisplay(dpy);
+#endif
+        std::exit(rc);
+}
+
+field_buffer_t toggle_lang()
+{
+        static constexpr field_buffer_t ltable[2] = {
+                {"US"},
+                {"RO"}
+        };
+        static constexpr const char* commands[2] = {
+                "setxkbmap us; setxkbmap -option numpad:mac",
+                "setxkbmap ro -variant std"
+        };
+        static bool flag = true;
+
+        flag = !flag;
+        std::system(commands[flag]);
+        return ltable[flag];
+}
+
+void setup()
 {
 #ifndef NO_X11
         dpy = XOpenDisplay(nullptr);
@@ -266,7 +274,7 @@ static void setup()
 #endif
 }
 
-static void handle_sig(const int sig)
+void handle_sig(const int sig)
 {
         const auto pred = [&](const auto& r)
         {
@@ -291,20 +299,7 @@ static void handle_sig(const int sig)
         it_group->second();
 }
 
-static void solve_signals()
-{
-        while(running)
-        {
-                if(last_sig != -1)
-                {
-                        handle_sig(last_sig);
-                }
-                set_root();
-                pause();
-        }
-}
-
-static bool already_running()
+bool already_running()
 {
 #ifndef IGNORE_ALREADY_RUNNING
         const auto cmdres1 = exec_cmd<true>("pgrep -x xsetstatus | wc -l");
@@ -316,6 +311,7 @@ static bool already_running()
         {
                 return true;
         }
+
         const auto cmdres2 = exec_cmd<true>("pgrep -x xsetstatus");
         return std::atoi(cmdres2.output.data()) != getpid();
 #else
@@ -323,17 +319,17 @@ static bool already_running()
 #endif
 }
 
-static void u_sig_handler(const int sig)
+void u_sig_handler(const int sig)
 {
         last_sig = sig;
 }
 
-static void terminator(const int)
+void terminator(const int)
 {
         running = 0;
 }
 
-static void init_signals()
+void init_signals()
 {
         for(const auto& r : sig_shell_responses)
         {
@@ -354,7 +350,7 @@ static void init_signals()
         signal(SIGINT, terminator);
 }
 
-static void init_statusbar()
+void init_statusbar()
 {
         for(const auto& r : rtable)
         {
@@ -364,6 +360,27 @@ static void init_statusbar()
         for(const auto& r : brtable)
         {
                 r.resolve();
+        }
+}
+
+void run_interval_responses()
+{
+        for(const auto& r : interval_responses)
+        {
+                r.resolve();
+        }
+}
+
+void solve_signals()
+{
+        while(running)
+        {
+                if(last_sig != -1)
+                {
+                        handle_sig(last_sig);
+                }
+                set_root();
+                pause();
         }
 }
 
