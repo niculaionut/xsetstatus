66346b2febf7656a3920103cafe17069b73138ff
Nicula Ionut <niculaionut@tutanota.com>
2021-07-16 13:16:13 +0300

    no -Werror

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 #flags
 CPPSTD = -std=c++20
-WFLAGS = -Wall -Wextra -Wpedantic -Werror
+WFLAGS = -Wall -Wextra -Wpedantic
 CPPFLAGS = ${CPPSTD} -O3 -march=native -flto -fno-exceptions -fno-rtti
 DEBUGFLAGS = ${CPPSTD} -DNO_X11 -DIGNORE_ALREADY_RUNNING -g -Og -march=native -fno-rtti -fno-exceptions -fno-omit-frame-pointer
 
